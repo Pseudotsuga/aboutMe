f634dfd4d0d65f634de9483889881d7feb4509ca
Conor <conormccue@gmail.com>
2019-09-24 16:04:43 -0700

    peace of mind event listener

--- js/app.js
+++ js/app.js
@@ -1,76 +1,80 @@
 'use strict';
 
+function renderPageFunction() {
 //Custom Greeting
-var username = prompt('What are you called?');
-alert('Hello ' + username + ' ! Thank you for your interest in my site.');
+  var username = prompt('What are you called?');
+  alert('Hello ' + username + ' ! Thank you for your interest in my site.');
 
-//First Question Block
-var noCrampons = prompt('Do I ice climb?', 'Yes/no or y/n answer formats accepted.');
+  //First Question Block
+  var noCrampons = prompt('Do I ice climb?', 'Yes/no or y/n answer formats accepted.');
 
-if(noCrampons.toLowerCase() === 'n' || noCrampons.toLowerCase() === 'no'){
-  alert('You\'re right, ice climbing would be a slippery slope towards year-round dirtbagging');
-}else{
-  alert('Better luck next time!');
-}
+  if(noCrampons.toLowerCase() === 'n' || noCrampons.toLowerCase() === 'no'){
+    alert('You\'re right, ice climbing would be a slippery slope towards year-round dirtbagging');
+  }else{
+    alert('Better luck next time!');
+  }
 
-// if(noCrampons.toLowerCase() === 'n' || noCrampons.toLowerCase() === 'no'){
-//   console.log('You\'re right, ice climbing would be a slippery slope towards year-round dirtbagging');
-// }else{
-//   console.log('Better luck next time!');
-// }
+  // if(noCrampons.toLowerCase() === 'n' || noCrampons.toLowerCase() === 'no'){
+  //   console.log('You\'re right, ice climbing would be a slippery slope towards year-round dirtbagging');
+  // }else{
+  //   console.log('Better luck next time!');
+  // }
 
-//Second Question Block
-var goPanthers = prompt('Did I graduate from Redmond High School?', 'Take your best guess!');
+  //Second Question Block
+  var goPanthers = prompt('Did I graduate from Redmond High School?', 'Take your best guess!');
 
-if(goPanthers.toLowerCase() === 'y' || goPanthers.toLowerCase() === 'yes'){
-  alert('You\'re right, let\'s go panthers!');
-}else{
-  alert('Better luck next time!');
-}
+  if(goPanthers.toLowerCase() === 'y' || goPanthers.toLowerCase() === 'yes'){
+    alert('You\'re right, let\'s go panthers!');
+  }else{
+    alert('Better luck next time!');
+  }
 
-// if(goPanthers.toLowerCase() === 'n' || goPanthers.toLowerCase() === 'no'){
-//   console.log('You\'re right, let\'s go panthers!');
-// }else{
-//   console.log('Better luck next time!');
-// }
+  // if(goPanthers.toLowerCase() === 'n' || goPanthers.toLowerCase() === 'no'){
+  //   console.log('You\'re right, let\'s go panthers!');
+  // }else{
+  //   console.log('Better luck next time!');
+  // }
 
-//Third Question Block
-var socrates = prompt('Do I know everything about software development?', 'Take your best guess!');
-if(socrates.toLowerCase() === 'n' || socrates.toLowerCase() === 'no'){
-  alert('You\'re right, I know just enough to get excited about how much I have to learn.');
-}else{
-  alert('Better luck next time!');
-}
+  //Third Question Block
+  var socrates = prompt('Do I know everything about software development?', 'Take your best guess!');
+  if(socrates.toLowerCase() === 'n' || socrates.toLowerCase() === 'no'){
+    alert('You\'re right, I know just enough to get excited about how much I have to learn.');
+  }else{
+    alert('Better luck next time!');
+  }
 
-// if(socrates.toLowerCase() === 'n' || socrates.toLowerCase() === 'no'){
-//   console.log('You\'re right, I know just enough to get excited about how much I have to learn.');
-// }else{
-//   console.log('Better luck next time!');
-// }
+  // if(socrates.toLowerCase() === 'n' || socrates.toLowerCase() === 'no'){
+  //   console.log('You\'re right, I know just enough to get excited about how much I have to learn.');
+  // }else{
+  //   console.log('Better luck next time!');
+  // }
 
-//Fourth Question Block
-var excelWizard = prompt('Have I always wanted to be an accountant?', 'Take your best guess!');
-if(excelWizard.toLowerCase() === 'n' || excelWizard.toLowerCase() === 'no'){
-  alert('You\'re right, I never quite got interested.');
-}else{
-  alert('Better luck next time!');
-}
+  //Fourth Question Block
+  var excelWizard = prompt('Have I always wanted to be an accountant?', 'Take your best guess!');
+  if(excelWizard.toLowerCase() === 'n' || excelWizard.toLowerCase() === 'no'){
+    alert('You\'re right, I never quite got interested.');
+  }else{
+    alert('Better luck next time!');
+  }
 
-// if(excelWizard.toLowerCase() === 'n' || excelWizard.toLowerCase() === 'no'){
-//   console.log('You\'re right, I never quite got interested.');
-// }else{
-//   console.log('Better luck next time!');
-// }
-//Fifth Question Block
-var occidentalis = prompt('Was I born in New York before moving west?', 'Take your best guess!');
-if(occidentalis.toLowerCase() === 'n' || occidentalis.toLowerCase() === 'no'){
-  alert('You\'re right, west coast best coast ' + username + '!');
-}else{
-  alert('Better luck next time ' + username + '!');
-}
+  // if(excelWizard.toLowerCase() === 'n' || excelWizard.toLowerCase() === 'no'){
+  //   console.log('You\'re right, I never quite got interested.');
+  // }else{
+  //   console.log('Better luck next time!');
+  // }
+  //Fifth Question Block
+  var occidentalis = prompt('Was I born in New York before moving west?', 'Take your best guess!');
+  if(occidentalis.toLowerCase() === 'n' || occidentalis.toLowerCase() === 'no'){
+    alert('You\'re right, west coast best coast ' + username + '!');
+  }else{
+    alert('Better luck next time ' + username + '!');
+  }
 
 // if(occidentalis.toLowerCase() === 'n' || occidentalis.toLowerCase() === 'no'){
 //   console.log('You\'re right, west coast best coast!');
 // }else{
 //   console.log('Better luck next time!');
 // }
+}
+// I added this section because I was getting annoyed by the prompt loading before the html content. It works like this: the window object supports the addEventListener method, this function takes as parameters an event type (load is one) and an function. In order to make this work I had to wrap all my javascript in a single function declaration.
+window.addEventListener('load', renderPageFunction);
