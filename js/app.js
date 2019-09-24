fa86e6cac52763eccd585016862f56a455ddaba1
Conor <conormccue@gmail.com>
2019-09-24 14:43:15 -0700

    Case validation working

--- js/app.js
+++ js/app.js
@@ -1,35 +1,35 @@
 'use strict';
 
 var noCrampons = prompt('Do I ice climb?', 'Yes/no or y/n answer formats accepted.');
-if(noCrampons === 'n' || noCrampons === 'no'){
+if(noCrampons.toLowerCase() === 'n' || noCrampons.toLowerCase() === 'no'){
   alert('You\'re right, ice climbing would be a slippery slope towards year-round dirtbagging');
 }else{
   alert('Better luck next time!');
 }
 
 var goPanthers = prompt('Did I graduate from Redmond High School?', 'Take your best guess!');
-if(goPanthers === 'y' || goPanthers === 'yes'){
+if(goPanthers.toLowerCase() === 'y' || goPanthers.toLowerCase() === 'yes'){
   alert('You\'re right, let\'s go panthers!');
 }else{
   alert('Better luck next time!');
 }
 
 var socrates = prompt('Do I know everything about software development?', 'Take your best guess!');
-if(socrates === 'n' || socrates === 'no'){
+if(socrates.toLowerCase() === 'n' || socrates.toLowerCase() === 'no'){
   alert('You\'re right, I know just enough to get excited about how much I have to learn.');
 }else{
   alert('Better luck next time!');
 }
 
 var excelWizard = prompt('Have I always wanted to be an accountant?', 'Take your best guess!');
-if(excelWizard === 'n' || excelWizard === 'no'){
+if(excelWizard.toLowerCase() === 'n' || excelWizard.toLowerCase() === 'no'){
   alert('You\'re right, I never quite got interested.');
 }else{
   alert('Better luck next time!');
 }
 
 var occidentalis = prompt('Was I born in New York before moving west?', 'Take your best guess!');
-if(occidentalis === 'n' || occidentalis === 'no'){
+if(occidentalis.toLowerCase() === 'n' || occidentalis.toLowerCase() === 'no'){
   alert('You\'re right, west coast best coast!');
 }else{
   alert('Better luck next time!');
