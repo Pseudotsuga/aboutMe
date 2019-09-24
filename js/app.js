649d0a558084448f2fef50f57c3a1bc7b44273b2
Conor <conormccue@gmail.com>
2019-09-24 14:56:22 -0700

    console logs added and commented out

--- js/app.js
+++ js/app.js
@@ -1,19 +1,35 @@
 'use strict';
-
+//First Question Block
 var noCrampons = prompt('Do I ice climb?', 'Yes/no or y/n answer formats accepted.');
+
 if(noCrampons.toLowerCase() === 'n' || noCrampons.toLowerCase() === 'no'){
   alert('You\'re right, ice climbing would be a slippery slope towards year-round dirtbagging');
 }else{
   alert('Better luck next time!');
 }
 
+// if(noCrampons.toLowerCase() === 'n' || noCrampons.toLowerCase() === 'no'){
+//   console.log('You\'re right, ice climbing would be a slippery slope towards year-round dirtbagging');
+// }else{
+//   console.log('Better luck next time!');
+// }
+
+//Second Question Block
 var goPanthers = prompt('Did I graduate from Redmond High School?', 'Take your best guess!');
+
 if(goPanthers.toLowerCase() === 'y' || goPanthers.toLowerCase() === 'yes'){
   alert('You\'re right, let\'s go panthers!');
 }else{
   alert('Better luck next time!');
 }
 
+// if(goPanthers.toLowerCase() === 'n' || goPanthers.toLowerCase() === 'no'){
+//   console.log('You\'re right, let\'s go panthers!');
+// }else{
+//   console.log('Better luck next time!');
+// }
+
+//Third Question Block
 var socrates = prompt('Do I know everything about software development?', 'Take your best guess!');
 if(socrates.toLowerCase() === 'n' || socrates.toLowerCase() === 'no'){
   alert('You\'re right, I know just enough to get excited about how much I have to learn.');
@@ -21,6 +37,13 @@ if(socrates.toLowerCase() === 'n' || socrates.toLowerCase() === 'no'){
   alert('Better luck next time!');
 }
 
+// if(socrates.toLowerCase() === 'n' || socrates.toLowerCase() === 'no'){
+//   console.log('You\'re right, I know just enough to get excited about how much I have to learn.');
+// }else{
+//   console.log('Better luck next time!');
+// }
+
+//Fourth Question Block
 var excelWizard = prompt('Have I always wanted to be an accountant?', 'Take your best guess!');
 if(excelWizard.toLowerCase() === 'n' || excelWizard.toLowerCase() === 'no'){
   alert('You\'re right, I never quite got interested.');
@@ -28,6 +51,12 @@ if(excelWizard.toLowerCase() === 'n' || excelWizard.toLowerCase() === 'no'){
   alert('Better luck next time!');
 }
 
+// if(excelWizard.toLowerCase() === 'n' || excelWizard.toLowerCase() === 'no'){
+//   console.log('You\'re right, I never quite got interested.');
+// }else{
+//   console.log('Better luck next time!');
+// }
+//Fifth Question Block
 var occidentalis = prompt('Was I born in New York before moving west?', 'Take your best guess!');
 if(occidentalis.toLowerCase() === 'n' || occidentalis.toLowerCase() === 'no'){
   alert('You\'re right, west coast best coast!');
@@ -35,4 +64,8 @@ if(occidentalis.toLowerCase() === 'n' || occidentalis.toLowerCase() === 'no'){
   alert('Better luck next time!');
 }
 
-// prompt the user a total of exactly five yes or no questions. The user input for the answer must accept either y/n or yes/no responses while taking into consideration case sensitivity by normalizing the user input so that it can be validated (hint: look into the .toUpperCase() or .toLowerCase() functions). Be sure to let the user know if they answered the question correctly by alerting them with a response.
+// if(occidentalis.toLowerCase() === 'n' || occidentalis.toLowerCase() === 'no'){
+//   console.log('You\'re right, west coast best coast!');
+// }else{
+//   console.log('Better luck next time!');
+// }
